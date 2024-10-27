7cfdb7f13c710f3b5c89f35ced23ffafd8d54f8c
oat9002 <oat9002@hotmail.com>
2024-10-27 16:33:09 +0700

    :construction:

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -7,6 +7,15 @@ import controllers.{AuthenticationRoute, WebHookRoute}
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration.DurationInt
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import cats.syntax.all._
+import com.comcast.ip4s.{IpLiteralSyntax, Port}
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.http4s.server.Router
 
 object Boot extends App {
   implicit val system: ActorSystem = ActorSystem()
@@ -14,22 +23,35 @@ object Boot extends App {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   val webHookRoute = WebHookRoute(executionContext, system)
 
-  val route =
-    concat(
-      pathSingleSlash {
-        get {
-          complete(
-            HttpEntity(ContentTypes.`application/json`,
-              "Say hello to akka-http"))
-        }
-      },
-      pathPrefix("webhook") {
-        withRequestTimeout(10.minutes)(webHookRoute.routes)
-      }
-    )
+//  val route =
+//    concat(
+//      pathSingleSlash {
+//        get {
+//          complete(
+//            HttpEntity(ContentTypes.`application/json`,
+//              "Say hello to akka-http"))
+//        }
+//      },
+//      pathPrefix("webhook") {
+//        withRequestTimeout(10.minutes)(webHookRoute.routes)
+//      }
+//    )
+  val helloRoute = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok("Hello world!")
+  }
 
-  val bindingFuture =
-    Http().newServerAt("localhost", Configuration.appConfig.port).bind(route)
+  val route = helloRoute
+  val httpApp = Router("/" -> route).orNotFound
+  val port = Port.fromInt(Configuration.appConfig.port).getOrElse(port"8080")
+  val server = EmberServerBuilder
+    .default[IO]
+    .withHost(ipv4"0.0.0.0")
+    .withPort(port)
+    .withHttpApp(httpApp)
+    .build
+
+  server.allocated.unsafeRunSync()._2
 
   println(s"Server online at http://localhost:${Configuration.appConfig.port}/")
 }
