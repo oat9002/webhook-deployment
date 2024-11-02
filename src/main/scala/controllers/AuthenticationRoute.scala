3c2272503f1f609d76a08ce7d84cc612fdef43bc
oat9002 <oat9002@hotmail.com>
2024-11-02 16:32:01 +0700

    :construction:

--- src/main/scala/controllers/AuthenticationRoute.scala
+++ src/main/scala/controllers/AuthenticationRoute.scala
@@ -1,21 +1,25 @@
 package controllers
 
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive, Route, StandardRoute}
+import cats.data.{Kleisli, OptionT}
+import cats.effect.{ExitCode, IO, IOApp}
 import common.Configuration
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.server.AuthMiddleware
+import org.typelevel.ci.CIStringSyntax
 
 class AuthenticationRoute {
-  val routes: Route = {
-    optionalHeaderValueByName("X-Api-Key") {
-      case Some(apiKey) if validate(apiKey) => reject
-      case _ => complete(StatusCodes.Unauthorized)
+  private val validateApiKey: Kleisli[IO, Request[IO], Either[String, Boolean]] = Kleisli { request =>
+    val isValid = request.headers.get(ci"X-API-KEY").map(_.head.value == Configuration.appConfig.apiKey)
+
+    isValid match {
+      case Some(true) => IO.pure(Right(true))
+      case _ => IO.pure(Left("Unauthorized"))
     }
   }
 
-  private def validate(apiKey: String): Boolean = {
-    apiKey.equals(Configuration.appConfig.apiKey)
-  }
+  val onFailure: AuthedRoutes[String, IO] = Kleisli(req => OptionT.pure(Response[IO](Status.Unauthorized).withEntity(req.context)))
+  val middleware: AuthMiddleware[IO, Boolean] = AuthMiddleware(validateApiKey, onFailure)
 }
 
 object AuthenticationRoute {
