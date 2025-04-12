b36e5a65fef19a620a038cfda9b1a558ab2eb48f
oat9002 <oat9002@hotmail.com>
2025-04-12 15:29:14 +0700

    :recycle:

--- src/main/scala/common/Commands.scala
+++ src/main/scala/common/Commands.scala
@@ -2,9 +2,9 @@ package common
 
 object Commands {
   val goldPriceTrackingDeploy: Seq[String] = List(
-    "sh ${Configuration.goldPriceTrackingConfig.folderPath}/server/deploy.sh"
+    s"sh ${Configuration.goldPriceTrackingConfig.folderPath}/server/deploy.sh"
   )
   val cryptoNotifyDeploy: Seq[String] = List(
-    "sh ${Configuration.cryptoNotifyConfig.folderPath}/deploy.sh"
+    s"sh ${Configuration.cryptoNotifyConfig.folderPath}/deploy.sh"
   )
 }
