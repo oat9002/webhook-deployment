bf58a4399f9f7d89400085c9ebd9674c5c146029
oat9002 <oat9002@hotmail.com>
2025-04-12 13:10:15 +0700

    :recycle:

--- src/main/scala/common/Commands.scala
+++ src/main/scala/common/Commands.scala
@@ -2,15 +2,9 @@ package common
 
 object Commands {
   val goldPriceTrackingDeploy: Seq[String] = List(
-    "docker stop server_gold-price-tracking-server_1",
-    "docker rm server_gold-price-tracking-server_1",
-    "docker image rm oat9002/gold-price-tracking-server",
-    s"docker-compose -f ${Configuration.goldPriceTrackingConfig.folderPath}/server/docker-compose.yml up -d"
+    "sh ${Configuration.goldPriceTrackingConfig.folderPath}/server/deploy.sh"
   )
   val cryptoNotifyDeploy: Seq[String] = List(
-    "docker stop crypto-notify_crypto-notify_1",
-    "docker rm crypto-notify_crypto-notify_1",
-    "docker image rm oat9002/crypto-notify",
-    s"${Configuration.cryptoNotifyConfig.folderPath}/docker-compose.yml up -d"
+    "sh ${Configuration.cryptoNotifyConfig.folderPath}/deploy.sh"
   )
 }
